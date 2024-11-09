eaba2c9c98f74ab08869583dd2d1630003f33428
ngocdiep2006 <2chidiepalz@proton.me>
2024-11-09 22:46:07 +0700

    first

---
+++ src/bin/logoCenter.ts
@@ -0,0 +1,21 @@
+/**
+ * logoCenter.ts - Căn logo, văn bản ra giữa console
+ * Viết bởi ngocdiep2006
+ */
+
+//@ts-check
+import * as readline from 'readline';
+
+export function logoCenter(logo: string): void {
+    
+    const lines = logo.split('\n');
+    const terminalWidth = process.stdout.columns;
+
+    const longestLineLength = Math.max(...lines.map(line => line.length));
+
+    const padding = Math.floor((terminalWidth - longestLineLength) / 2);
+
+    lines.forEach(line => {
+        console.log(' '.repeat(padding) + line);
+    });
+}
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import * as webdriver from 'selenium-webdriver'
+import { logoCenter } from './bin/logoCenter'
+
+const logo = `
+╔═══════════════════════════════════╗
+║     Auto Collect Luckyvn Coins    ║
+╚═══════════════════════════════════╝
+   https://github.com/ngocdiep2006
+            RELEASE-v1
+
+`
+
+console.log(logoCenter(logo))
